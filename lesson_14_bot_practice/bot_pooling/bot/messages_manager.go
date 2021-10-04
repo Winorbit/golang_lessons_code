2ef9ced99de1809563a3df5b85c52b7daba509fd
getman1 <fadetoblacknow@gmail.com>
2021-10-04 13:04:06 +0300

    add piece of code

--- lesson_14_bot_practice/bot_pooling/bot/messages_manager.go
+++ lesson_14_bot_practice/bot_pooling/bot/messages_manager.go
@@ -1,48 +1,59 @@
 package bot
 
-import ("net/http"
-        "encoding/json"
-        "fmt"
-        "net/url"
-        "strconv"
-      )
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
 
 func contains(elements []string, element string) bool {
-  for _, el := range elements {
-    if el == element {
-      return true
-    }
-  }
-  return false
+	for _, el := range elements {
+		if el == element {
+			return true
+		}
+	}
+	return false
 }
 
+func GetUpdates(token string) Updates {
+	updatesUrl := fmt.Sprintf("%s%s/%s", rootURL, token, "getUpdates")
+	resp, _ := http.Get(updatesUrl)
 
-func getUpdates(token string) Updates {
-  ...
+	var updates Updates
+	decoder := json.NewDecoder(resp.Body)
+	decoder.Decode(&updates)
+	return updates
 }
 
+func SendMessage(chatId string, message string, token string) {
+	sendMessageURL := fmt.Sprintf("%s%s/sendMessage", rootURL, token)
+	res, err := http.PostForm(sendMessageURL,
+		url.Values{"chat_id": {chatId},
+			"text": {message}})
 
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
-func sendMessage(chatId string, message string, token string) {
-  sendMessageURL := fmt.Sprintf("%s%s/sendMessage", rootURL, token)
-  res, err := http.PostForm(sendMessageURL,
-                            url.Values{"chat_id": {chatId},
-                                       "text": {message} })
-  statusCode := res.Status[0:3]
+	statusCode := res.Status[0:3]
 
-  ...
-  }
+	codeOk := contains(okCodes, statusCode)
 
+	if codeOk != true {
+		fmt.Printf("Request failed with statuscode %s", statusCode)
+	}
 
-func processMessages(chatId string, message string, token string){
-  if message == "Hello"{
-    sendMessage(chatId, "Your welcome!", token)
-  } else {
-    sendMessage(chatId, message, token)
-  }
 }
 
+// func processMessages(chatId string, message string, token string){
+//   if message == "Hello"{
+//     sendMessage(chatId, "Your welcome!", token)
+//   } else {
+//     sendMessage(chatId, message, token)
+//   }
+// }
 
-func Pooling(token string){
-  ...
-}
+// func Pooling(token string){
+//   ...
+// }
